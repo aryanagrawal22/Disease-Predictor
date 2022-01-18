a73b2d27157e4b6db5786b75451480ab804991b0
aryanagrawal22 <aryanagrawal20023@gmail.com>
2022-01-19 02:08:17 +0530

    port modified

--- server.js
+++ server.js
@@ -294,6 +294,6 @@ app.post("/hepatitis_prediction", function (req, res) {
     });
 });
 
-app.listen(3000, function () {
-  console.log("Server is running on port 3000");
+app.listen(process.env.PORT || 3000 , function () {
+  console.log("Server is running");
 });
